31448af3095d56ceb6876b3ccae88b9f4320bd41
Mayur <mayurkanade59@gmail.com>
2024-03-03 18:00:50 +0530

    Added For and While loop

---
+++ 06/script.js
@@ -0,0 +1,24 @@
+// Loops
+
+// For loop : if you know for how many times to loop.
+// you know how many iterations will be there then use for loop.
+for (let i = 1; i <= 10; i++) {
+  console.log("Mayur " + i);
+}
+
+/*
+   you                    house
+   how many steps did you require to reach house?
+   that is if you don't know how many iterations are required then we use while loop
+*/
+
+let initialPosition = 0;
+let house = 50;
+
+while (initialPosition != house) {
+  initialPosition++;
+  console.log("Steps taken: " + initialPosition);
+}
+
+/*
+ */
