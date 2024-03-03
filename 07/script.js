80cc4eb55eae911f577efd7effe16c57ec14770a
Mayur <mayurkanade59@gmail.com>
2024-03-03 21:49:27 +0530

    Multiple Arguments passed while calling the function using arguments

---
+++ 07/script.js
@@ -0,0 +1,39 @@
+// Function
+
+// function sayHello() {
+//   console.log(`Mayur Kanade`);
+// }
+
+// sayHello();
+// sayHello();
+// sayHello();
+// sayHello();
+// sayHello();
+
+/* Multiply two numbers in JS */
+
+// function mulTwoNums(num1, num2) {
+//   return num1 * num2;
+// }
+
+// function add(num1, num2) {
+//   return num1 + num2;
+// }
+
+// let ans = mulTwoNums(4, 5);
+// console.log(ans);
+// let addResult = add(5, 5);
+// console.log(addResult + 1);
+
+// add function for n number of parameters
+
+function addParaNumbers() {
+  let ans = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    ans += arguments[i];
+  }
+  return ans;
+}
+
+let result = addParaNumbers(12, 222, 557, 987, 789);
+console.log(result);
