368665ebf1e2d9f51f93f7b70699530f6ed83ded
Mayur <mayurkanade59@gmail.com>
2024-03-03 22:51:47 +0530

    The hoisting is possible in the Normal function

--- 08/script.js
+++ 08/script.js
@@ -1,49 +1,59 @@
 // Arrow Functions
 
-// 1. Syntax
-function sayHello() {
-  console.log("Normal Function Hello");
-}
-sayHello();
+// // 1. Syntax
+// function sayHello() {
+//   console.log("Normal Function Hello");
+// }
+// sayHello();
 
-// ES6 Arrow function
+// // ES6 Arrow function
 
-let sayBye = () => {
-  // Arrow function
-  console.log("Bella Ciao!!");
-};
-
-sayBye();
-
-// const add = (a, b) => {
-//   // arguments in Arrow functions.
-//   return a + b;
+// let sayBye = () => {
+//   // Arrow function
+//   console.log("Bella Ciao!!");
 // };
 
-// OR single statement
+// sayBye();
 
-const add_v2 = (a, b) => a + b;
+// // const add = (a, b) => {
+// //   // arguments in Arrow functions.
+// //   return a + b;
+// // };
 
-console.log(add_v2(4, 5)); // one liner function
+// // OR single statement
 
-// 2. 'arguments' keyword
-/* The arguments keyword is not available in the Arrow function.
- The arguments keyword is only available in normal function. 
- However, the spread argument is possible in the Arrow functions.
- */
-// eg:
+// const add_v2 = (a, b) => a + b;
 
-let addNumbers = (...numbers) => {
-  let ans = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    ans += numbers[i];
-  }
-  console.log(ans);
-};
+// console.log(add_v2(4, 5)); // one liner function
 
-let result = addNumbers(55, 55, 100);
-console.log(result);
+// // 2. 'arguments' keyword
+// /* The arguments keyword is not available in the Arrow function.
+//  The arguments keyword is only available in normal function.
+//  However, the spread argument is possible in the Arrow functions.
+//  */
+// // eg:
+
+// let addNumbers = (...numbers) => {
+//   let ans = 0;
+//   for (let i = 0; i < numbers.length; i++) {
+//     ans += numbers[i];
+//   }
+//   console.log(ans);
+// };
+
+// let result = addNumbers(55, 55, 100);
+// console.log(result);
 
 // 3. Hoisting
+// Hoisting is possible only for the normal functions but not for the Arrow functions.
+
+// hoisting example for the normal function.
+
+sayHey(); // it will execute in the normal function
+
+function sayHey() {
+  // this would be created first in the memory.
+  console.log("Hello There");
+}
 
 // 4. this keyword
