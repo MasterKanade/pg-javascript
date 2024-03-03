cea18c78cdf3078e3cdef1d32377c7810a435463
Mayur <mayurkanade59@gmail.com>
2024-03-03 22:47:43 +0530

    Arrow Functions

---
+++ 08/script.js
@@ -0,0 +1,49 @@
+// Arrow Functions
+
+// 1. Syntax
+function sayHello() {
+  console.log("Normal Function Hello");
+}
+sayHello();
+
+// ES6 Arrow function
+
+let sayBye = () => {
+  // Arrow function
+  console.log("Bella Ciao!!");
+};
+
+sayBye();
+
+// const add = (a, b) => {
+//   // arguments in Arrow functions.
+//   return a + b;
+// };
+
+// OR single statement
+
+const add_v2 = (a, b) => a + b;
+
+console.log(add_v2(4, 5)); // one liner function
+
+// 2. 'arguments' keyword
+/* The arguments keyword is not available in the Arrow function.
+ The arguments keyword is only available in normal function. 
+ However, the spread argument is possible in the Arrow functions.
+ */
+// eg:
+
+let addNumbers = (...numbers) => {
+  let ans = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    ans += numbers[i];
+  }
+  console.log(ans);
+};
+
+let result = addNumbers(55, 55, 100);
+console.log(result);
+
+// 3. Hoisting
+
+// 4. this keyword
