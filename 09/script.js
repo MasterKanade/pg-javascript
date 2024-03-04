cd81c4123d26531b8bee0b8380e5b4d3a3309743
Mayur <mayurkanade59@gmail.com>
2024-03-04 09:43:22 +0530

    High-Order and Callback Example By Piyush

--- 09/script.js
+++ 09/script.js
@@ -1,11 +1,23 @@
 /* Function that takes a function as an argument */
 // High order function and callback.
 
-let add = (num1, num2) => num1 + num2; // this is my high order function and passing it into anoterh fun as an argument.
+// let add = (num1, num2) => num1 + num2; // this is my high order function and passing it into anoterh fun as an argument.
 
-let addThreeNumbers = (first, second, third, sum) => {
-  let result = sum(first, second);
-  return result + third;
-};
+// let addThreeNumbers = (first, second, third, sum) => {
+//   let result = sum(first, second);
+//   return result + third;
+// };
 
-console.log(addThreeNumbers(1, 2, 3, add));
+// console.log(addThreeNumbers(1, 2, 3, add));
+
+// High Order function and callback example
+
+// this function is called high-order function because it is accepting function as a parameter.
+function add(a, b, callback) {
+  let result = a + b;
+  callback(result);
+}
+
+add(2, 4, (val) =>
+  console.log("Passed the value from function add callback function:", val)
+);
