f978ab055c78835475177677380101b7fcbe9299
Mayur <mayurkanade59@gmail.com>
2024-03-04 13:54:19 +0530

    spice method

--- 11/script.js
+++ 11/script.js
@@ -21,27 +21,40 @@ console.log(newArr); */
 // let newStorage = numbers.map((val) => val * 2); // here it returns the new array.
 // console.log(newStorage);
 
-let numStore = [2, 4, 6, 8]; //  this is myArray
+//let numStore = [2, 4, 6, 8, 10, 11, 13, 15, 9, 21]; //  this is myArray
 
-function square(num) {
-  // Square Function
-  return num * num;
-}
+// function square(num) {
+//   // Square Function
+//   return num * num;
+// }
 
-let forEachSquare = numStore.forEach((val) => square(val));
-console.log(forEachSquare); // forEach does not return any value. output : undefined.
+// let forEachSquare = numStore.forEach((val) => square(val));
+// console.log(forEachSquare); // forEach does not return any value. output : undefined.
 
-let mapSquare = numStore.map((val) => square(val));
-console.log(mapSquare); // map returns the value into newArray. output: [4,16,36,64]
+// let mapSquare = numStore.map((val) => square(val));
+// console.log(mapSquare); // map returns the value into newArray. output: [4,16,36,64]
 
-let findNumber = numStore.find((val) => val === 4);
-console.log(findNumber); // true if present the number and if not present then it's undefined.
+// let findNumber = numStore.find((val) => val === 4);
+// console.log(findNumber); // true if present the number and if not present then it's undefined.
 
-let findIndexOfNumber = numStore.findIndex((val) => val === 4);
-console.log(findIndexOfNumber); // true if present the number and if not present then it's -1
+// let findIndexOfNumber = numStore.findIndex((val) => val === 4);
+// console.log(findIndexOfNumber); // true if present the number and if not present then it's -1
 
-let isPresent = numStore.includes(4);
-console.log(isPresent); // if number is present it will return true and if not present then it will be false.
+// let isPresent = numStore.includes(4);
+// console.log(isPresent); // if number is present it will return true and if not present then it will be false.
 
-let filterEven = numStore.filter((val) => val % 2 === 0);
-console.log(filterEven);
+// let filterEven = numStore.filter((val) => val % 2 === 0);
+// console.log(filterEven);
+
+let numStore = [2, 4, 6, 8, 10, 11, 12, 25];
+let subArray = numStore.slice(0, 4); // formula is start position and (n-1) element would be return
+console.log(subArray);
+let ignoreEnd = numStore.slice(1);
+console.log(ignoreEnd); // from start position to till end of the array would be return.
+console.log(numStore); // The slice method does not directly change the original array.
+
+// Javascript splice method.
+
+let delNumbers = numStore.splice(1, 3); // start from 1st position and n-1 elements. (4,6,8) would be deleted.
+console.log(delNumbers);
+console.log(numStore); // now the original array changed as [2, 10, 11, 12, 25]
