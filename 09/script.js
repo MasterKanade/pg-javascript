f115f11c1d17495bae8ee8212ec8ac4ef3a9c5db
Mayur <mayurkanade59@gmail.com>
2024-03-04 09:54:51 +0530

    returning function from function

--- 09/script.js
+++ 09/script.js
@@ -13,11 +13,23 @@
 // High Order function and callback example
 
 // this function is called high-order function because it is accepting function as a parameter.
-function add(a, b, callback) {
-  let result = a + b;
+// function add(a, b, callback) {
+//   let result = a + b;
+//   callback(result);
+// }
+
+// add(2, 4, (val) =>
+//   console.log("Passed the value from function add callback function:", val)
+// );
+
+// add(400, 100, (res) => console.log(res));
+
+function add(num1, num2, callback) {
+  let result = num1 + num2;
   callback(result);
+
+  return () => console.log(result); // return a function from a function.
 }
 
-add(2, 4, (val) =>
-  console.log("Passed the value from function add callback function:", val)
-);
+let functionResult = add(2, 12, () => {}); // calling function into the function as an argument dynamically.
+functionResult();
