73623928add3f7bb7cde3ce8ba80a8ec072f48d0
Mayur <mayurkanade59@gmail.com>
2024-03-04 10:57:05 +0530

    forEach method in arrays acts similar to callback functions

---
+++ 11/script.js
@@ -0,0 +1,4 @@
+/* The forEach function in JS acts as a callback function over the array */
+
+let students = ["Mayur", "Rishabh", "Ajay", "Mahendra", "Yuvraj"];
+students.forEach((val) => console.log(val + " Cricket Icon"));
