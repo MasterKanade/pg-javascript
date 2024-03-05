c502ed7e4fdb2d87d800d5065a6d996b011124ef
Mayur <mayurkanade59@gmail.com>
2024-03-05 18:31:04 +0530

    Onclick Page Load functionality in LocalStorage

--- localStorage/script.js
+++ localStorage/script.js
@@ -5,6 +5,12 @@ const setText = document.getElementById("startHeader");
 buttonEle.addEventListener("click", () => {
   const result = userInput.value;
   localStorage.setItem("userInput", result);
+  location.reload(); // location is by default object and on click page will be reloaded.
+});
+
+window.addEventListener("load", () => {
   const putText = localStorage.getItem("userInput");
   setText.innerText = putText;
+  setText.style.background = "lightslategrey";
+  setText.style.color = "chartreuse";
 });
